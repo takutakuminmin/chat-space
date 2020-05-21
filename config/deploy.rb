2c2cf28a97cf354472f47b9dc3a8c53bb50cd26b
takutakuminmin <takutakuminmin0730@icloud.com>
2020-05-21 19:21:11 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,6 +27,12 @@ set :linked_files, %w{ config/secrets.yml }
 
 # 元々記述されていた after 「'deploy:publishing', 'deploy:restart'」以下を削除して、次のように書き換え
 
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+
   desc 'upload secrets.yml'
   task :upload do
     on roles(:app) do |host|
